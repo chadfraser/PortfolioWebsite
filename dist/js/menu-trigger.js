42972ccfea061ab3963d34e8745cb65ad4ceae28
Chad Fraser <chad.fraser@mail.mcgill.ca>
2019-06-25 13:29:21 -0600

    Clean up menu and update animation

--- dist/js/menu-trigger.js
+++ dist/js/menu-trigger.js
@@ -1,9 +1,9 @@
 const menuTab = document.querySelector(".menu-tab");
 const menuButton = document.querySelector(".menu-button");
-const buttonLine = document.querySelector(".button-line");
+const buttonLines = document.querySelectorAll(".button-line");
 const navMenu = document.querySelector(".nav-menu");
 const menuList = document.querySelector(".menu-list");
-const menuItem = document.querySelectorAll(".menu-item");
+const menuItems = document.querySelectorAll(".menu-item");
 
 let menuVisible = false;
 
@@ -12,18 +12,20 @@ menuTab.addEventListener("click", toggleMenuVisible);
 function toggleMenuVisible() {
     if (!menuVisible) {
         menuTab.classList.add("close");
-        menuButton.classList.add("close");
+        // menuButton.classList.add("close");
+        // buttonLines.forEach(item => item.classList.add("close"));
         navMenu.classList.add("visible");
         menuList.classList.add("visible");
-        menuItem.forEach(item => item.classList.add("visible"));
+        menuItems.forEach(item => item.classList.add("visible"));
 
         menuVisible = true;
     } else {
         menuTab.classList.remove("close");
-        menuButton.classList.remove("close");
+        // menuButton.classList.remove("close");
+        // buttonLines.forEach(item => item.classList.remove("close"));
         navMenu.classList.remove("visible");
         menuList.classList.remove("visible");
-        menuItem.forEach(item => item.classList.remove("visible"));
+        menuItems.forEach(item => item.classList.remove("visible"));
 
         menuVisible = false;
     }
