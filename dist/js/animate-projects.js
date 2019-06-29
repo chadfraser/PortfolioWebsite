5523045f9492316c3b65dc92539017c0ce1d36a0
Chad Fraser <chad.fraser@mail.mcgill.ca>
2019-06-28 18:39:55 -0600

    Simply animation code to reset gifs on mouse exit

--- dist/js/animate-projects.js
+++ dist/js/animate-projects.js
@@ -1,20 +1,20 @@
 const projectSamples = document.querySelectorAll(".project-sample");
 const projectImages = document.querySelectorAll(".project-image");
 const projectAnimations = document.querySelectorAll(".project-animation");
+var imageSourceList = [];
 
 projectSamples.forEach(function (sample, index) { 
     var image = projectImages[index];
     var animation = projectAnimations[index];
-    sample.addEventListener("mouseenter", function() { showAnimation(image, animation) });
-    sample.addEventListener("mouseleave", function() { hideAnimation(image, animation) });
+    sample.addEventListener("mouseenter", function() { showAnimation(index) });
+    sample.addEventListener("mouseleave", function() { hideAnimation(index) });
+    imageSourceList.push([image.src, animation.src]);
 });
 
-function showAnimation(image, animation) {
-    image.classList.remove("visible");
-    animation.classList.add("visible");
+function showAnimation(index) {
+    projectImages[index].src = imageSourceList[index][1];
 }
 
-function hideAnimation(image, animation) {
-    image.classList.add("visible");
-    animation.classList.remove("visible");
+function hideAnimation(index) {
+    projectImages[index].src = imageSourceList[index][0];
 }
