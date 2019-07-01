eedd625ac1bb57dc1b04739260fbf71b5775ffc0
Chad Fraser <chad.fraser@mail.mcgill.ca>
2019-06-30 19:53:02 -0600

    Add default BOLD theme

--- dist/js/change-theme.js
+++ dist/js/change-theme.js
@@ -1,16 +1,23 @@
 settingsItems.forEach(item => item.addEventListener("click", function() { changeTheme(item); }));
 
 var prev_handler = window.onload;
-window.onload = updateAllElementThemeClasses;
+window.onload = function() {
+    setInitialTheme();
+    updateAllElementThemeClasses();
+}
+
+function setInitialTheme() {
+    if (!localStorage.getItem("theme")) {
+        localStorage.setItem("theme", "theme--bold");
+    }
+}
 
 function changeTheme(item) {
     localStorage.setItem("theme", item.id);
-    // console.log(localStorage);
     updateAllElementThemeClasses();
 }
 
 function updateAllElementThemeClasses() {
-    // console.log("AAA", localStorage);
     var allElements = document.body.getElementsByTagName("*");
     for (var i = 0; i < allElements.length; i++) {
         removePreviousThemeClass(allElements[i]);
@@ -18,18 +25,14 @@ function updateAllElementThemeClasses() {
     }
     removePreviousThemeClass(document.body);
     addCurrentThemeClass(document.body);
-    // console.log(localStorage);
-    // console.log(allElements);
 }
 
 function removePreviousThemeClass(elem) {
-    // console.log(elem.classList);
     elem.classList.forEach(className => {
         if (className.startsWith("theme--")) {
             elem.classList.remove(className);
         }
     });
-    // console.log(elem.classList);
 }
 
 function addCurrentThemeClass(elem) {
