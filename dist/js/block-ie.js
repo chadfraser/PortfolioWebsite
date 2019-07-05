3e3df50121e18c9028c1fcc6bb6194373ee5f3f5
Chad Fraser <chad.fraser@mail.mcgill.ca>
2019-07-05 13:14:54 -0600

    Replace single quotes with double quotes for consistency

--- dist/js/block-ie.js
+++ dist/js/block-ie.js
@@ -1,7 +1,7 @@
 function isInternetExplorer() {
     var ua = window.navigator.userAgent;
-    var msie = ua.indexOf('MSIE ');
-    var trident = ua.indexOf('Trident/');
+    var msie = ua.indexOf("MSIE ");
+    var trident = ua.indexOf("Trident/");
 
     return (msie > 0 || trident > 0);
 }
